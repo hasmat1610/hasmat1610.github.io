93fb8f64057ae613b9c43ee2b4e56bc91658b00e
hasmat1610 <58145017+hasmat1610@users.noreply.github.com>
2021-01-17 21:02:14 +0530

    Add js & image
    Add js & image

---
+++ hasmat/assets/js/custom.js
@@ -0,0 +1,88 @@
+function openNav() {
+  $("#mySidenav").addClass("width80");
+  $("#nav-res").addClass("opacityon");
+  $(".cd-shadow-layer").addClass("displayblock");
+  $(".wrapper").addClass("position-fixed-custom");
+  $("body").addClass("overflow-fixed");
+}
+
+function closeNav() {
+  $("#mySidenav").removeClass("width80");
+  $("#nav-res").removeClass("opacityon");
+  $(".cd-shadow-layer").removeClass("displayblock");
+  $(".wrapper").removeClass("position-fixed-custom");
+  $("body").removeClass("overflow-fixed");
+}
+
+$(document).ready(function () {
+  $(".cd-shadow-layer").click(function () {
+    closeNav();
+  });
+
+  /* ===============================  Scroll back to top  =============================== */
+
+  ("use strict");
+
+  var progressPath = document.querySelector(".progress-wrap path");
+  var pathLength = progressPath.getTotalLength();
+  progressPath.style.transition = progressPath.style.WebkitTransition = "none";
+  progressPath.style.strokeDasharray = pathLength + " " + pathLength;
+  progressPath.style.strokeDashoffset = pathLength;
+  progressPath.getBoundingClientRect();
+  progressPath.style.transition = progressPath.style.WebkitTransition =
+    "stroke-dashoffset 10ms linear";
+  var updateProgress = function () {
+    var scroll = $(window).scrollTop();
+    var height = $(document).height() - $(window).height();
+    var progress = pathLength - (scroll * pathLength) / height;
+    progressPath.style.strokeDashoffset = progress;
+  };
+  updateProgress();
+  $(window).scroll(updateProgress);
+  var offset = 150;
+  var duration = 550;
+  jQuery(window).on("scroll", function () {
+    if (jQuery(this).scrollTop() > offset) {
+      jQuery(".progress-wrap").addClass("active-progress");
+    } else {
+      jQuery(".progress-wrap").removeClass("active-progress");
+    }
+  });
+  jQuery(".progress-wrap").on("click", function (event) {
+    event.preventDefault();
+    jQuery("html, body").animate({ scrollTop: 0 }, duration);
+    return false;
+  });
+
+  /* ===============================  End of Scroll back to top  =============================== */
+
+  $(window).scroll(function () {
+    var sticky = $(".header-div"),
+      scroll = $(window).scrollTop();
+
+    if (scroll >= 190) {
+      sticky.addClass("header-bgcolor slideInDown animated");
+    } else {
+      sticky.removeClass("header-bgcolor slideInDown animated");
+    }
+  });
+});
+
+$(window).on("load", function () {
+  /* ===============================  isotope Masonery  =============================== */
+  $(".gallery").isotope({
+    itemSelector: ".items",
+  });
+
+  var $gallery = $(".gallery").isotope();
+
+  $(".filtering").on("click", "span", function () {
+    var filterValue = $(this).attr("data-filter");
+    $gallery.isotope({ filter: filterValue });
+  });
+
+  $(".filtering").on("click", "span", function () {
+    $(this).addClass("active").siblings().removeClass("active");
+  });
+  /* ===============================  End of isotope Masonery  =============================== */
+});
