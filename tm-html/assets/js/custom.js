d190674a47d9139f97917094f6593925a65b13e3
Hasmat Patel <58145017+hasmat1610@users.noreply.github.com>
2021-12-04 20:25:19 +0530

    Add files via upload

--- tm-html/assets/js/custom.js
+++ tm-html/assets/js/custom.js
@@ -20,7 +20,6 @@ $(document).ready(function () {
     $(".backdrop-menu-layer").removeClass("active");
     $(".nav-dashboard-right").removeClass("active");
   });
-
   /* End of mobile menu */
 
   /* Desktop collapse menu */
@@ -35,7 +34,31 @@ $(document).ready(function () {
       $(".left-side-menu").removeClass("text-fixed-remove");
     }, 200);
   });
+
+  $("#menu-collpese-open").click(function () {
+      $(".left-side-nav-main-root").toggleClass("active").toggleClass("active-hg-left-root");
+      $("#main-dashboard").toggleClass("active").toggleClass("active-hg-main-dashboard");
+  });
+
   /* End of desktop collapse menu */
 
   $("[data-toggle='tooltip']").tooltip();
+
+  /* File Upload */
+  var readURL = function (input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $(".profile-pic").attr("src", e.target.result);
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  };
+  $(".file-upload").on("change", function () {
+    readURL(this);
+  });
+  $(".upload-button").on("click", function () {
+    $(".file-upload").click();
+  });
+  /* End of File Upload */
 });
